eea7c5b9f4cb415e79b84e6fe7ea9b9cbc701fb8
Brad Windsor <bwindsor22@gmail.com>
2020-02-23 14:11:27 -0500

    update run train

--- models/train_dqn.py
+++ models/train_dqn.py
@@ -75,47 +75,44 @@ def run_training_for_params(policy_net,
 
     memory = ReplayMemory(10000)
     results = []
-    for i_episode in range(EPISODES):
-        print("Episode #: ", i_episode)
-        env.reset()
-        last_screen = get_screen(env, device)
+    env.reset()
+    last_screen = get_screen(env, device)
+    current_screen = get_screen(env, device)
+    state = current_screen - last_screen
+    sum_score = 0
+
+    for t in count():
+        action = select_action(state, policy_net)
+        _, reward, done, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+
+
+        # Observe new state
+        last_screen = current_screen
         current_screen = get_screen(env, device)
-        state = current_screen - last_screen
-        sum_score = 0
+        if not done:
+            next_state = current_screen - last_screen
+        else:
+            next_state = None
 
-        for t in count():
-            action = select_action(state, policy_net)
-            _, reward, done, _ = env.step(action.item())
-            reward = torch.tensor([reward], device=device)
+        sum_score += reward
+        #         if t % 100 == 0:
+        #             print("Time: ", t, " Reward: ", reward[0], "Score: ", sum_score[0], " Aliens Killed: ",aliens_killed)
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
 
-
-            # Observe new state
-            last_screen = current_screen
-            current_screen = get_screen(env, device)
-            if not done:
-                next_state = current_screen - last_screen
-            else:
-                next_state = None
-
-            sum_score += reward
-            #         if t % 100 == 0:
-            #             print("Time: ", t, " Reward: ", reward[0], "Score: ", sum_score[0], " Aliens Killed: ",aliens_killed)
-            # Store the transition in memory
-            memory.push(state, action, next_state, reward)
-
-            # Move to the next state
-            state = next_state
-            if done:
-                # plot_durations()
-                if reward == 2:
-                    print("WIN \n" * 10)
-                    print("Score: ", sum_score.item(), " Aliens Killed: ", aliens_killed)
-                    results.append([sum_score.item(), aliens_killed, 1])
-                elif reward == -1:
-                    print("LOSE \n" * 10)
-                    print("Score: ", sum_score.item(), " Aliens Killed: ", aliens_killed)
-                    results.append([sum_score.item(), aliens_killed, 0])
-                break
+        # Move to the next state
+        state = next_state
+        if done:
+            if reward == 2:
+                print("WIN \n" * 10)
+                print("Score: ", sum_score.item(), " Aliens Killed: ", aliens_killed)
+                results.append([sum_score.item(), 1])
+            elif reward == -1:
+                print("LOSE \n" * 10)
+                print("Score: ", sum_score.item(), " Aliens Killed: ", aliens_killed)
+                results.append([sum_score.item(), 0])
+            break
 
     print('Complete')
     final_result = results[-1]
@@ -137,6 +134,6 @@ if __name__ == '__main__':
         init_model = [screen_height, screen_width, LINEAR_INPUT_SCALAR, KERNEL, n_actions]
         policy_net = DQN(*init_model).to(device)
         return policy_net, init_model
-    net = get_initial_policy_net()
+    net, model = get_initial_policy_net()
 
-    run_training_for_params()
+    run_training_for_params(net, model)
