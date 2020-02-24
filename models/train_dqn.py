f9702c360ba1baa2b24a5cf8be1ee2ee5ddb1560
Brad Windsor <bwindsor22@gmail.com>
2020-02-23 22:47:38 -0500

    Revert "add cache for environment maker"
    This reverts commit f92d35f24b462e0142594985cd803d66cde2009b.

--- models/train_dqn.py
+++ models/train_dqn.py
@@ -46,11 +46,9 @@ def select_action(state, policy_net, n_actions,
 
 
 def run_training_for_params(policy_net,
-                            game_level,
-                            env_maker=None):
+                            game_level):
     logging.info('making level %s', game_level)
-
-    env = env_maker.make(game_level) if env_maker else gym.make(game_level)
+    env = gym.make(game_level)
 
     global steps_done
     steps_done = 0
