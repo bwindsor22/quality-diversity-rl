10e96ab57f025d612cc9084554283614e2e86c6e
Brad Windsor <bwindsor22@gmail.com>
2020-03-08 15:17:43 -0400

    tested maker

--- test/test_caching_env_maker.py
+++ test/test_caching_env_maker.py
@@ -1,8 +1,8 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 from models.caching_environment_maker import CachingEnvironmentMaker, GVGAI_RUBEN, GVGAI_BAN4D
 
 class CachingEnvMakerTest(TestCase):
-
+    @skip('not currently testing')
     def test_ruben(self):
         EnvMaker = CachingEnvironmentMaker(GVGAI_RUBEN)
         for _ in range(100):
@@ -10,7 +10,7 @@ class CachingEnvMakerTest(TestCase):
                 env = EnvMaker.make('gvgai-aliens-lvl{}-v0'.format(lvl))
                 self.assertIsNotNone(env)
 
-    def test_(self):
+    def test_ban4d(self):
         EnvMaker = CachingEnvironmentMaker(GVGAI_BAN4D)
         for _ in range(100):
             for lvl in range(5):
