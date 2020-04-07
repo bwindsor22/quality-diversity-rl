2d6169dd46df3899cfa0195e692ff12f68fcd5cf
Brandon O'Shea <boshea93@gmail.com>
2020-04-06 22:34:02 -0400

    Modified install_bam4d script

--- install_bam4d.sh
+++ install_bam4d.sh
@@ -4,12 +4,12 @@
 # if so, try conda install -c conda-forge/label/gcc7 openjdk 
 
 echo "creating environment"
-python3 -m venv qd-bam4d-env
+python -m venv qd-bam4d-env
 source qd-bam4d-env/bin/activate
 
 echo "installing basic files"
-pip3 install flatbuffers==1.11
-pip3 install -r requirements.txt
+pip install flatbuffers==1.11
+pip install -r requirements.txt
 
 echo "installing gym"
 GYM_DIR=GVGAI_GYM_BAM
@@ -18,5 +18,5 @@ mkdir $GYM_DIR
 cd $GYM_DIR
 git clone https://github.com/Bam4d/GVGAI_GYM.git
 cd GVGAI_GYM/python/gvgai
-pip3 install -e .
+pip install -e .
 cd ../../../../
