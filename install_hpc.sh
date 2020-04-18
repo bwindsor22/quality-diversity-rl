08d875d7d305cdc74430609147ffa8db233f3bf9
Brad Windsor <bwindsor22@gmail.com>
2020-04-18 14:01:00 -0400

    install hpc

--- install_hpc.sh
+++ install_hpc.sh
@@ -1,12 +1,2 @@
 echo "loading modules"
-module load gcc/6.3.0
-module load python3/intel/3.7.3
-
-echo "installing jdk"
-source /share/apps/anaconda3/5.3.1/etc/profile.d/conda.sh
-conda create -n qd-rl-0
-conda activate qd-rl-0
-conda install -c conda-forge/label/gcc7 openjdk
-
-echo "running normal install"
-bash install_bam4d.sh
+source /scratch/bw1879-share/py3.7/bin/activate
