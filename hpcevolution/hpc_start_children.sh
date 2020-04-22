37b16d5f9707ce269a609352fe9b8c077c957bfe
bwindsor22 <bwindsor22@gmail.com>
2020-04-21 23:51:39 -0400

    add start children

---
+++ hpcevolution/hpc_start_children.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        --agents)
+            AGENTS=$VALUE
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+echo "agents is $AGENTS ";
+
+for i in $(seq $AGENTS)
+do
+    echo "starting sbatch $i" &
+    sbatch run_child.sbatch &
+done
+
