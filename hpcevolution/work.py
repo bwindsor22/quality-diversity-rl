5097b53ae51e1b97e2d7bd92f96306f14b23a654
boshea93 <34881009+boshea93@users.noreply.github.com>
2020-07-23 23:00:58 -0400

    Add number of training levels and objectives

--- hpcevolution/work.py
+++ hpcevolution/work.py
@@ -1,7 +1,9 @@
 class Work:
-    def __init__(self, model, score_strategy,game,stop_after,run_name):
+    def __init__(self, model, score_strategy,game,stop_after,run_name,num_levels,objectives):
         self.model = model
         self.score_strategy = score_strategy
         self.game = game
         self.stop_after = stop_after
         self.run_name = run_name
+        self.num_levels = num_levels
+        self.objectives = objectives
